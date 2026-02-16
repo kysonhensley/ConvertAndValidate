9108d45e67f14bc1974206f29baea68fd68268bd
kysonhensley <hensleykyson@gmail.com>
2026-02-15 20:42:30 -0700

    create convert and validate method

--- ConvertAndValidate/Program.cs
+++ ConvertAndValidate/Program.cs
@@ -6,5 +6,7 @@
         {
             Console.WriteLine("Hello, World!"); 
         }
+
+        //create method that when called will convert a string to an int and return result, if the string cannot be converted then return error message
     }
 }
